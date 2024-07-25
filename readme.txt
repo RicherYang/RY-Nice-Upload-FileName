baebc539018a8a9db35945b48a38f87e4c669b9c
Richer Yang <fantasyworldidvtw@gmail.com>
2024-07-25 10:38:03 +0800

    更新外掛資訊

--- readme.txt
+++ readme.txt
@@ -4,8 +4,8 @@ Donate link: https://www.paypal.me/RicherYang
 Tags: upload, filename
 Requires at least: 5.6
 Requires PHP: 7.4
-Tested up to: 6.2
-Stable tag: 1.0.7.1
+Tested up to: 6.6
+Stable tag: 1.0.8
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.txt
 
@@ -29,6 +29,9 @@ Report bugs on the [GitHub repository](https://github.com/RicherYang/RY-Nice-Upl
 or my [website page](https://ry-plugin.com/ry-nice-upload-filename/forum).
 
 == Changelog ==
+
+= 1.0.8 - 2024/07/25 =
+* Change plugin info.
 
 = 1.0.7 - 2023/03/09
 * Fix upload file name without extension error.
--- ry-nice-upload-filename.php
+++ ry-nice-upload-filename.php
@@ -3,7 +3,7 @@
  * Plugin Name: RY Nice Upload FileName
  * Plugin URI: https://ry-plugin.com/ry-nice-upload-filename
  * Description: Rewrite upload filename if not english or number letter
- * Version: 1.0.7.1
+ * Version: 1.0.8
  * Requires at least: 5.6
  * Requires PHP: 7.4
  * Author: Richer Yang
@@ -13,8 +13,8 @@
 
 function_exists('plugin_dir_url') or exit('No direct script access allowed');
 
-add_filter('sanitize_file_name', 'RY_NUFN_sanitize_file_name');
-function RY_NUFN_sanitize_file_name($file_name)
+add_filter('sanitize_file_name', 'RY_NUFN_sanitize_file_name', 10, 2);
+function RY_NUFN_sanitize_file_name($filename, $filename_raw)
 {
     $parts = explode('.', $file_name);
     if (1 == count($parts)) {
@@ -24,7 +24,7 @@ function RY_NUFN_sanitize_file_name($file_name)
         $file_name = implode('.', $parts);
     }
     if (!preg_match('@^[a-z0-9][a-z0-9\-_\.]*$@i', $file_name)) {
-        $file_name = substr(md5($file_name), 0, 10);
+        $file_name = substr(md5($filename_raw), 0, 10);
     }
     if ('' !== $extension) {
         $file_name .= '.' . $extension;
